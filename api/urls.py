daa6080ff2b224b4a0a5c93962e34f0a57994534
mendesbayout <62385787+mendesbayout@users.noreply.github.com>
2021-03-22 18:06:48 -0300

    master4

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
+
+#Esses urls vão ser usados pelo swagger para fazer requests na db
 urlpatterns = [
-    #path('cheff/', views.Cheffs.as_view()),
-    #path('cheff/<str:cheff_id>/', views.CheffsDetail.as_view()),
-    #path('cheff/<str:cheff_id>/recipes/', views.Recipes.as_view()),
-    #path('cheff/<str:cheff_id>/recipes/<str:recipe_id>/', views.RecipeDetail.as_view()),
-    #path('cheff/<str:cheff_id>/recipes/<str:recipe_id>/<str:type>', views.RecipeDetail.as_view()),
+    path('cheff/', views.Cheffs.as_view()),
+    path('cheff/<str:cheff_id>/', views.CheffsDetail.as_view()),
+    path('cheff/<str:cheff_id>/recipes/', views.Recipes.as_view()),
 ]
--- cheff/urls.py
+++ cheff/urls.py
@@ -9,19 +9,18 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
    openapi.Info(
       title="Cheff API backend exam WORKALOVE",
-      
+      subtitle="Cheff API backend exam WORKALOVE",        
       default_version='beta backend',
-      description="Esta interface é a conexão entre você e dois dos melhores cheff's do mundo, um é especialista em FastFood, seu nome é Vinsmoke, o outro, é especialista em receitas demoradas, ele se chama Zeff, você comprou esse contato direto com eles por 5 milhões de dolares. Essa API fornece também a possibilidade de integração com Qualquer sistema de frontend.Caso tenha algum problema, acesse o dominio http://127.0.0.1:8000/admin/, as credenciais devem ser criadass",
+      description="Esta interface é a conexão entre você e dois dos melhores cheff's do mundo, um é ESPECIALISTA EM FASTFOOD, Vinsmoke, o outro, é ESPECIALISTA EM RECEITAS demoradas, ele se chama Zeff, você comprou esse contato direto com eles por 5 milhões de dolares. Essa API fornece também a possibilidade de integração com Qualquer sistema de frontend.Caso tenha algum problema, acesse o dominio http://127.0.0.1:8000/admin/, as credenciais devem ser criadas.",
       terms_of_service="https://github.com/mendesbayout",
       contact=openapi.Contact(email="mendesbayout@gmail.com"),
-      license=openapi.License(name="Como o test é de backend voltado para Django Rest, acho que um Swagger funcional pode ser chamado de CRUD Interface"),
+      license=openapi.License(name="Como o test é de backend voltado para Django Rest, acho que um Swagger funcional pode ser chamado de CRUD Interface. 1 - GET>TRY>Execute to meet their Ids and go on"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
     path('api', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
